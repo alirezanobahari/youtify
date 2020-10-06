54e9dc8aa1d80ada83c43453cf39e35ff91c0c62
alireza <alireza.ra1996@gmail.com>
2020-10-06 21:37:03 +0330

    Update keys.js

--- src/common/keys.js
+++ src/common/keys.js
@@ -1,3 +1,2 @@
-export const GOOGLE_API_KEY = "AIzaSyDyDzhLDnIGEtbxzSFh2dOPd5uUr8xq4H0";
-export const TELEGRAM_BOT_TOKEN =
-  "819732172:AAFNeBY51kOnT0mezTLa9OetGkoljWxNsfE";
+export const GOOGLE_API_KEY = "";
+export const TELEGRAM_BOT_TOKEN = "";
